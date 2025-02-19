88e9c86678bfc516c5cdbca91da22b4dd1b56c9d
MusaKunroo <148885731+MusaKunroo@users.noreply.github.com>
2025-02-20 00:16:19 +0530

    Add files via upload

---
+++ 11.MathFuncs.py
@@ -0,0 +1,8 @@
+import math #Here we imported math module to acces many mathematical functions
+x=-2.99
+print(round(x))
+print(abs(x))
+print(math.ceil(x)) #Prints smallest integer greater than or equal to a given number.
+print(math.floor(x)) #Opposite of ceiling
+
+#!There are many other Math moducle function you can explore
